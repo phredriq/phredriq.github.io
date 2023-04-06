23d347018b793a8b9ddc1601c616c672554806d1
unknown <freli918@student.liu.se>
2023-04-06 15:16:38 +0200

    working Ok

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
-    // Kod i detta block körs när dokumentet laddats klart.   
-    alert("hej"); 
-    $("#mainViewContainer").html($("#homepage").html())
+       $("#mainViewContainer").html($("#homepage").html())
    
 })
+
+
+
+function SwitchToAboutUs () {
+    document.getElementById('bodyelement').style.backgroundImage="url(img/iskallnew2.png)";
+    $("#mainViewContainer").html($("#omoss").html())
+
+}
+
+
+function SwitchTohome() {
+    document.getElementById('bodyelement').style.backgroundImage="url(img/iskallnew.png)";
+    $("#mainViewContainer").html($("#homepage").html())
+}
